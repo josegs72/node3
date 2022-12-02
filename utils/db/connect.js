8159279b8edf595371fe69f731f6707eb7bf592a
josegs72 <josegs72@gmail.com>
2022-12-02 10:02:10 +0100

    first commit

--- utils/db/connect.js
+++ utils/db/connect.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 // Almacenamos en una constante la url obtenida desde Mongo Atlas. (Sustituimos contraseña por la del usuario).
-const DB_URL = "mongodb+srv://root:GKNM0qCJZ5Sjz7uN@moneyheist.8rg8c8y.mongodb.net/?retryWrites=true&w=majority";
-
+const DB_URL = "mongodb+srv://root:6jXaFh7kGbY4brzD@cluster0.jqruqhx.mongodb.net/?retryWrites=true&w=majority";
 const connect = () => {
     // La función connect permite conectar nuestro servidor a MongoDB
     // 1. Url base de datos
